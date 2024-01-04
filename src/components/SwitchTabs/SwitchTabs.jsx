6faae24f59d507d2b5f56eff1eeb0e9e9043cc85
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-01-04 13:55:03 +0600

    added switch tabs

---
+++ src/components/SwitchTabs/SwitchTabs.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./SwitchTabs.scss";
+
+function SwitchTabs({ data, onTabChange }) {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const activeTab = (tab, index) => {
+    setLeft(index * 100);
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 300);
+    onTabChange(index, tab);
+  };
+
+  return (
+    <div className="switchingTabs">
+      <div className="tabItems">
+        {data.map((tab, index) => (
+          <span
+            key={index}
+            className={`tabItem ${selectedTab === index ? "active" : ""}`}
+            onClick={() => activeTab(tab, index)}
+          >
+            {tab}
+          </span>
+        ))}
+        <span className="movingBg" style={{ left }} />
+      </div>
+    </div>
+  );
+}
+
+export default SwitchTabs;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,15 +6,14 @@ import Banner from "./Banner/Banner";
 import { useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Trending from "./Trending/Trending";
 
 const Home = () => {
-  const location = useLocation();
-
-  const noHeaderFooter =
-    location.pathname.includes("login") || location.pathname.includes("signUp");
   return (
     <div className="homePage">
       <Banner />
+      <Trending />
+      <div style={{ height: 500 }}></div>
     </div>
   );
 };
